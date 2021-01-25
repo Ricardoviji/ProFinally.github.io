77d026c2c47f63225a483748fc10870b05b2fe41
Ricardoviji <77933507+Ricardoviji@users.noreply.github.com>
2021-01-25 00:41:26 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBvCcvb1rP9n5fpcGuTF6TFfrF1nrp8Ke0",
+    authDomain: "profinal-78278.firebaseapp.com",
+    projectId: "profinal-78278",
+    storageBucket: "profinal-78278.appspot.com",
+    messagingSenderId: "827672167724",
+    appId: "1:827672167724:web:561b916ba522e292ac9889",
+    measurementId: "G-6TXXWBTFCE"
+  });
